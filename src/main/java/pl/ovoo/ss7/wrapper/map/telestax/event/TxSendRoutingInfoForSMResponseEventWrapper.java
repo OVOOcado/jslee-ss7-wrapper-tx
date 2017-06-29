47e7ce6959175689023168bd72ef647e626f0308
Karol Simka <karol.simka@ovoo.pl>
2017-06-29 19:09:08 +0200

    Fix for networkNodeNumber

--- src/main/java/pl/ovoo/ss7/wrapper/map/telestax/event/TxSendRoutingInfoForSMResponseEventWrapper.java
+++ src/main/java/pl/ovoo/ss7/wrapper/map/telestax/event/TxSendRoutingInfoForSMResponseEventWrapper.java
@@ -2,6 +2,7 @@ package pl.ovoo.ss7.wrapper.map.telestax.event;
 
 import org.mobicents.protocols.ss7.map.api.service.callhandling.SendRoutingInformationResponse;
 import org.mobicents.protocols.ss7.map.api.service.sms.SendRoutingInfoForSMResponse;
+import org.mobicents.protocols.ss7.map.primitives.ISDNAddressStringImpl;
 import pl.ovoo.ss7.wrapper.common.telestax.TxIMSIAddressWrapper;
 import pl.ovoo.ss7.wrapper.common.telestax.TxISDNAddressStringWrapperImpl;
 import pl.ovoo.ss7.wrapper.map.args.SendRoutingInfoForSMResponseWrapper;
@@ -27,6 +28,7 @@ public class TxSendRoutingInfoForSMResponseEventWrapper extends TxMapEventWrappe
     public SendRoutingInfoForSMResponseWrapper getArgument() {
         TxSendRoutingInfoForSMResponseWrapper sri = new TxSendRoutingInfoForSMResponseWrapper();
         sri.setImsi(new TxIMSIAddressWrapper(sendRoutingInfoForSMResponse.getIMSI()));
+        sri.setMscAddress(new TxISDNAddressStringWrapperImpl(sendRoutingInfoForSMResponse.getLocationInfoWithLMSI().getNetworkNodeNumber()));
         return sri;
     }
 
